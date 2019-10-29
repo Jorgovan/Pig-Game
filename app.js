b7a2f7fc7ca487bf316502e461a220b6cb8cd278
Matej Bošnjak <49959490+Jorgovan@users.noreply.github.com>
2019-10-29 23:00:41 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
             dice1DOM.style.display='block';
             dice1DOM.src = 'dice-' + dice1 + '.png';
             dice2DOM.style.display='block';
-            dice2DOM.src = 'dice-' + dice1 + '.png';
+            dice2DOM.src = 'dice-' + dice2 + '.png';
             
             if (dice1===1 || dice2 === 1) {
                 scores[activePlayer]=0;
