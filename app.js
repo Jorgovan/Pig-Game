1fe4e9e4c8c449863825f0ed4b62836480d1977e
Matej Bošnjak <49959490+Jorgovan@users.noreply.github.com>
2019-10-29 23:04:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -115,10 +115,7 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
                 
             }
 
-            
-            
-            
-            
+      
         }
 
     }
